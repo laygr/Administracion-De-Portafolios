a3a5d0322c9d4606da0fef4225d2b0db5d8f4964
Jesús Irais González Romero <laygr@outlook.com>
2017-09-06 18:31:17 -0500

    Blacklitterman & expectations inclusion

---
+++ AdministracionDePortafolios/BlackLitterman.fs
@@ -0,0 +1,19 @@
+﻿module BlackLitterman
+
+open Optimizacion
+
+let (*) m1 m2 = MatrixOp.mmult(m1, m2)
+let (/) m1 m2 = MatrixOp.pointwiseDivision(m1, m2)
+let (+) m1 m2 = MatrixOp.addition(m1, m2)
+
+let ( *^ ) m1 (f:float) = MatrixOp.mmultbyscalar(m1, f)
+let (/^) m1 f = MatrixOp.dividebyscalar(m1, f)
+let (+^) m1 (f:float) = MatrixOp.addScalar(m1, f)
+
+let withNormalizingFactor (weights:float[]) (varcovar:float[,]) (anticipatedBenchmarkReturn:float) (otherThing:float) : float[,] =
+        let weightsM = MatrixOp.matrixFrowRow weights
+        (
+        ((varcovar * (MatrixOp.transpose(weightsM))) *^ (otherThing - anticipatedBenchmarkReturn))
+        /^
+        (weightsM * varcovar * (MatrixOp.transpose(weightsM))).[0,0]
+        ) +^ anticipatedBenchmarkReturn
--- AdministracionDePortafolios/Program.fs
+++ AdministracionDePortafolios/Program.fs
@@ -9,6 +9,9 @@ module Main =
     let omega = loadMatrix "..\..\Omega.csv" false
     let expectedReturns = loadRow "..\..\ExpectedReturns.csv" false
     let initialValues = loadRow "..\..\InitialValues.csv" false
+
+    let benchmarkOmega = loadMatrix "..\..\BenchmarkOmega.csv" false
+    let benchmarkWeights = loadRow "..\..\BenchmarkWeights.csv" false
     
     [<EntryPoint>]
     let main argv =
@@ -16,7 +19,15 @@ module Main =
         let result = opt.Opt(initialValues)
         result.Variables
         |> Array.iter (printfn "%f")
-        printfn "%A" argv
         let arr = array2D [[2.;1.];[1.;2.]]
         let inverse = MatrixOp.invert arr
+
+        let x = BlackLitterman.withNormalizingFactor benchmarkWeights benchmarkOmega 0.0633 0.08
+        let adjustedReturns =
+            ExpectationsInclusion.adjustedReturns
+                [|0.074455629; 0.070676728; 0.07313265; 0.081624918; 0.111186519; 0.082504604; 0.082040492; 0.077847073; 0.075568456; 0.074764192|]
+                [|0.005; 0.;0.;0.;-0.01; 0.005;0.;0.;0.;0.01|]
+                benchmarkOmega
+            
+        printfn "%A" argv
         0 // devolver un código de salida entero
